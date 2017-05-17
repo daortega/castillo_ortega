1bebd8c2c80fe59d4ef38cedc6cedcb4b16a8f5e
Denisse Ortega <daortega@uc.cl>
2017-05-17 14:07:00 -0400

    ups

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+<footer>
+<p>Macarena Castillo + Denisse Ortega · Miércoles 17 de mayo, 2017 · <a href="https://github.com/daortega/castillo_ortega">Ver en repositorio Github</a></p>
+</footer>
+</div>
+<script src="libs/js/jquery.min.js"></script>
+<script src="libs/js/bootstrap.min.js"></script>
+</script>
+</body>
+</html>
